a333fae1656e9d4cefa006d1510319030b2c4e18
LuisanaHenmary <luisanahenmaryp@gmail.com>
2024-05-13 17:04:42 -0400

    Layaout con menu

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,24 +1,177 @@
 import { Outlet } from "react-router-dom";
 import { imprimirLog } from "../utils/test";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getUsers } from "../api/testAPi";
+import { styled, useTheme } from '@mui/material/styles';
+
+import {
+  Button
+} from "@mui/material";
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+
+
+
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+
+import ListItemText from '@mui/material/ListItemText';
+
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+
 
 const Layout = () => {
+  const [open, setOpen] = useState(false);
+  const theme = useTheme();
 
-  useEffect(()=>{
+  useEffect(() => {
+
     getUsers()
-    .then(data => {
-      const {data: results} = data
-      console.log(results);
-    })
-    .catch(error => console.log(error))
+      .then(data => {
+        const { data: results } = data
+        console.log(results);
+      })
+      .catch(error => console.log(error))
   })
 
   return (
-    <>
-      <h1>Layout</h1>
-      <Outlet />
-    </>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <Button
+            color="inherit"
+            aria-label="open drawer"
+            onClick={()=>setOpen(true)}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            Menu
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <Button onClick={()=>setOpen(false)}>
+            x
+          </Button>
+          <Typography variant="h6" color={"red"} >
+            MundoVinyl
+          </Typography>
+        </DrawerHeader>
+
+        <Divider />
+        <List>
+          <h4>Módulos</h4>
+          {['Compras', 'Ventas'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+
+        <Divider />
+
+        <List>
+        <h4>Catalogos</h4>
+          {['Clientes', 'Productos', 'Proveedores'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+
+        <Divider />
+
+        <List>
+        <h4>Usuarios</h4>
+          {['Empleados', 'Roles', 'Salir'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+
+      <Main open={open}>
+        <DrawerHeader />
+        <Outlet />
+      </Main>
+
+      
+    </Box>
   );
 };
 
