a91e828fb43f011b5e108d614f07bb1023785d13
LuisanaHenmary <luisanahenmaryp@gmail.com>
2024-05-14 16:24:31 -0400

    Agregado componente empleados

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,6 +3,7 @@ import Layout from "../layout/Layout"
 import Compras from "../pages/Compras"
 import Roles from "../pages/Roles"
 import Clientes from "../pages/Clientes"
+import Empleados from "../pages/Empleados"
 
  const router = createBrowserRouter([
     { path: '/login', element: <div>Login</div>, },
@@ -15,7 +16,7 @@ import Clientes from "../pages/Clientes"
             { path: '/clientes', element: <Clientes/>, },
             { path: '/productos', element: <div>productos</div>, },
             { path: '/proveedores', element: <div>proveedores</div>, },
-            { path: '/empleados', element: <div>empleados</div>, },
+            { path: '/empleados', element: <Empleados/>, },
             { path: '/roles', element: <Roles />, },
       
         ]
