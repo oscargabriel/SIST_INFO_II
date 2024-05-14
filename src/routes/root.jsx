86998e6f21de937994ece1fa6dbeea5dfe29ee8c
LuisanaHenmary <luisanahenmaryp@gmail.com>
2024-05-14 12:13:38 -0400

    Agregada la pagina de roles

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
 import Layout from "../layout/Layout"
 import Compras from "../pages/Compras"
+import Roles from "../pages/Roles"
 
  const router = createBrowserRouter([
     { path: '/login', element: <div>Login</div>, },
@@ -14,7 +15,7 @@ import Compras from "../pages/Compras"
             { path: '/productos', element: <div>productos</div>, },
             { path: '/proveedores', element: <div>proveedores</div>, },
             { path: '/empleados', element: <div>empleados</div>, },
-            { path: '/roles', element: <div>roles</div>, },
+            { path: '/roles', element: <Roles />, },
       
         ]
     }
