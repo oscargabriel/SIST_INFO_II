f5fa04330f33badc6e33c2c570799b52bca73778
LuisanaHenmary <luisanahenmaryp@gmail.com>
2024-05-17 18:25:59 -0400

    Creada tabla de empleados y status reutilisables

---
+++ src/utils/status.js
@@ -0,0 +1,16 @@
+export const employerStatus = [
+    {
+        name: 'Disponible',
+        sx: {backgroundColor:'#38BF22', color: 'white', padding:"5px", borderRadius:2, fontWeight:'bold'},
+    },
+
+    {
+        name: 'Vacaciones',
+        sx: {backgroundColor:'#33BAB2', color: 'white', padding:"5px", borderRadius:2, fontWeight:'bold'},
+    },
+    {
+        name: 'Ocupado',
+        sx: {backgroundColor:'#BA3333', color: 'white', padding:"5px", borderRadius:2, fontWeight:'bold'},
+    },
+]
+
