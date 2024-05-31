c435caf34155c653e7354ade7fa691b9e18be7ee
LuisanaHenmary <luisanahenmaryp@gmail.com>
2024-05-31 11:25:07 -0400

    Agregados estados para proveedores

--- src/utils/status.js
+++ src/utils/status.js
@@ -30,3 +30,19 @@ export const ClientStatus = [
     },
 ]
 
+export const SupplierStatus = [
+    {
+        name: 'Estado1',
+        sx: {backgroundColor:'#38BF22', color: 'white', padding:"5px", borderRadius:2, fontWeight:'bold'},
+    },
+
+    {
+        name: 'Estado2',
+        sx: {backgroundColor:'#33BAB2', color: 'white', padding:"5px", borderRadius:2, fontWeight:'bold'},
+    },
+    {
+        name: 'Estado3',
+        sx: {backgroundColor:'#BA3333', color: 'white', padding:"5px", borderRadius:2, fontWeight:'bold'},
+    },
+]
+
